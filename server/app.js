3b4ffa7e331715b1aea320e62f266a15837b9a64
VictorSogamosoWW <victor.sogamoso.05@gmail.com>
2025-10-06 15:39:41 -0500

    Separacion de rutas y server

---
+++ server/app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const routes = require('../routes/routes');
+
+//Usar rutas
+app.use(routes);
+
+// Iniciar el servidor
+app.listen(3000, ()=> {console.log('Server is running on port 3000');});
