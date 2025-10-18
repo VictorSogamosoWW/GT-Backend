6db1ab0fbb3d12458030a64ef45a10576825cf32
VictorSogamosoWW <victor.sogamoso.05@gmail.com>
2025-10-18 14:16:01 -0500

    Conexion a base de datos Atlas

--- server/app.js
+++ server/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
 const routes = require('../routes/routes');
+const conectarDB = require('../db');
 
 //Usar rutas
 app.use(routes);
+
+// Conectar a la base de datos
+conectarDB();
 
 // Iniciar el servidor
 app.listen(3000, ()=> {console.log('Server is running on port 3000');});
